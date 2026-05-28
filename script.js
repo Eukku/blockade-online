a69d6b589598c3ac1aaf673761a11a53b8c80bb8
Eukku <111589723+Eukku@users.noreply.github.com>
2026-05-28 22:13:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fetchData() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://blockade3d.com/api_classic/servers/handler.php?NETWORK=1&CMD=4&time=1', true);
+    xhr.open('GET', 'https://little-mouse-9ca4.pravo669719.workers.dev/', true);
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
